7c0fb22235e6d29e14f533efb0ae67373c2d7fc7
JadenFicklin <jadendficklin@gmail.com>
2022-06-30 23:44:42 -0600

    setup register with database

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url: "http://localhost:5000/api/register",
+      data: {
+        username: username,
+        password: password,
+      },
+    })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
       <h1>Hello! Whats your name brave one?</h1>
+      <form onSubmit={handleSubmit}>
+        <h2>create your character</h2>
+        <input
+          type="text"
+          placeholder="name"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br></br>
+        <input
+          type="password"
+          placeholder="passcode"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br></br>
+        <button>submit</button>
+      </form>
     </>
   );
 }
